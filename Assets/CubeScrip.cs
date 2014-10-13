d04af3a9bb80105d88d08ad0e51654639eeda140
Patrycja Tomaszewska <patricia.97@wp.pl>
2014-10-13 14:55:36 +0200

    Made the Cube move
    Made the Cube move

---
+++ Assets/CubeScrip.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScrip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//if (Input.GetKeyDown("f")) {
+			StartCoroutine("Move");
+		//}
+
+	}
+
+	IEnumerator Move()
+	{
+		for (int i=0; i <= 200; i++) 
+		{
+			transform.Translate(Vector3.forward * Time.deltaTime);
+
+			yield return null;
+		}
+	}
+}
